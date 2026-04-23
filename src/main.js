36f645ba09d5ca0c0b0026f9bf9c65f6936b4b44
Anastasiia Pototska <delphano86@gmail.com>
2026-04-23 13:49:10 +0200

    hw correction

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import {
   hideLoadMoreButton 
 } from "./js/render-functions.js";
 
-const form = document.querySelector('.search-form');
+const form = document.querySelector('.form');
 const loadMoreBtn = document.querySelector('.load-more');
 
 let currentQuery = '';
